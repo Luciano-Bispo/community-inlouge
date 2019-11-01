10e3b4c4d25d307d8688ce27525da6e8448e222b
Carllotta <carllottadias@gmail.com>
2019-11-01 16:26:05 -0300

    Warning removido

--- api-comil/Models/communityInLoungeContext.cs
+++ api-comil/Models/communityInLoungeContext.cs
@@ -27,7 +27,6 @@ namespace api_comil.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=communityInLounge;Integrated Security=true;");
             }
         }
